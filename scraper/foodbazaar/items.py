8e5d93bd1e3ecdf80658209f5b53df5181d18fc1
rivka-yuliia <juliacreadora@gmail.com>
2024-07-18 15:47:29 +0300

    items

--- scraper/foodbazaar/items.py
+++ scraper/foodbazaar/items.py
@@ -1,12 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from itemloaders.processors import Join  # noqa
 
 
-class FoodbazaarItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class FoodbazaarItem(scrapy.Item):  # noqa
+    name = scrapy.Field(output_processor=Join())
+    price = scrapy.Field(output_processor=Join())
+    link = scrapy.Field(output_processor=Join())
