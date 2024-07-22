db9f5d85272221eb1d56d6e25fed089e95c96bc6
rivka-yuliia <juliacreadora@gmail.com>
2024-07-22 23:54:51 +0300

    scrape all links

--- scraper/foodbazaar/spiders/food.py
+++ scraper/foodbazaar/spiders/food.py
@@ -2,22 +2,27 @@ from typing import Iterable
 
 import scrapy
 from scrapy import Request
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
 from scrapy.selector import Selector
 
 from foodbazaar.items import FoodbazaarItem
 
 from scrapy_selenium import SeleniumRequest
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By  # noqa
 
 import time
 
 
-class FoodSpider(scrapy.Spider):
+class FoodSpider(CrawlSpider):
     name = "food"
     allowed_domains = ["shop.foodbazaar.com"]
-    start_urls = ["https://shop.foodbazaar.com/store/food-bazaar/collections/n-fresh-fruits-51483"]
+    start_urls = ["https://shop.foodbazaar.com/store/food-bazaar/storefront"]
+    link_extractor = LinkExtractor(
+        restrict_xpaths='//nav[@id="sm-departments"]',
+        process_value=lambda x: f'https://shop.foodbazaar.com{x}'
+    )
 
     def start_requests(self) -> Iterable[Request]:
         yield SeleniumRequest(
@@ -28,6 +33,8 @@ class FoodSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         driver = response.meta['driver']
+        driver.get('https://shop.foodbazaar.com/store/food-bazaar/storefront')
+
         btn_cookies = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
         btn_cookies.click()
         time.sleep(2)
@@ -35,38 +42,56 @@ class FoodSpider(scrapy.Spider):
         btn_zip.click()
         time.sleep(2)
 
-        i = 1
-        num_scrolls = 2
-        last_height = driver.execute_script("return document.body.scrollHeight")
+        a_elements = driver.find_elements(
+            By.XPATH,
+            '//nav[@aria-labelledby="sm-departments"]//a'
+        )
+        departments = [x.get_attribute('href') for x in a_elements]
 
-        while True and i <= num_scrolls:
-            i += 1
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(3)
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-
-        r = Selector(text=driver.page_source)
-
-        containers = r.xpath('//div[@class="e-13udsys"]')
-
-        for container in containers[1:]:
-            item = ItemLoader(
-                item=FoodbazaarItem(),
-                response=r,
-                selector=container
+        for dp in departments:
+            driver.get(dp)
+            category_elements = driver.find_elements(
+                By.XPATH,
+                '//ul[@class="e-1jkorx6"]//a[@class="e-18z2zel"]'
             )
+            links = [x.get_attribute('href') for x in category_elements]
 
-            item.add_xpath('name', './/h2/text()')
-            item.add_xpath(
-                'price',
-                './/div[@class="e-1s49gp4"]/span[1]/text()'
-            )
-            item.add_xpath(
-                'link',
-                './/a[@class="e-1dlf43s"]/@href'
-            )
+            for link in links:
+                driver.get(link)
+                time.sleep(2)
 
-            yield item.load_item()
+                i = 1
+                num_scrolls = 2
+                last_height = driver.execute_script("return document.body.scrollHeight")
+
+                while True and i <= num_scrolls:
+                    i += 1
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    time.sleep(3)
+                    new_height = driver.execute_script("return document.body.scrollHeight")
+                    if new_height == last_height:
+                        break
+                    last_height = new_height
+
+                r = Selector(text=driver.page_source)
+
+                containers = r.xpath('//div[@class="e-13udsys"]')
+
+                for container in containers[1:]:
+                    item = ItemLoader(
+                        item=FoodbazaarItem(),
+                        response=r,
+                        selector=container
+                    )
+
+                    item.add_xpath('name', './/h2/text()')
+                    item.add_xpath(
+                        'price',
+                        './/div[@class="e-1s49gp4" or @class="e-1jioxed"]/span[1]/text()'
+                    )
+                    item.add_xpath(
+                        'link',
+                        './/a[@class="e-1dlf43s"]/@href'
+                    )
+
+                    yield item.load_item()
