f2398c6025f4fb5986d0f6335d8f4941e53dbc39
rivka-yuliia <juliacreadora@gmail.com>
2024-07-24 02:43:18 +0300

    table style to separate methods

--- scraper/foodbazaar/pipelines.py
+++ scraper/foodbazaar/pipelines.py
@@ -50,31 +50,13 @@ class AddToXlsxPipeline:
             self.worksheet.append(header)
 
             # Add style to header
-            for letter in letters:
-                self.worksheet[letter].fill = PatternFill(
-                    'solid',
-                    fgColor='dfe4ff'
-                )
-                self.worksheet[letter].font = Font(
-                    bold=True,
-                    name='Arial',
-                    size=12
-                )
-                self.worksheet[letter].alignment = Alignment(
-                    horizontal='center',
-                    vertical='center'
-                )
-                self.worksheet[letter].border = Border(
-                    top=Side(border_style='thin', color='000000'),
-                    right=Side(border_style='thin', color='000000'),
-                    bottom=Side(border_style='thin', color='000000'),
-                    left=Side(border_style='thin', color='000000')
-                )
+            self.add_header_style(letters)
 
             # Add value below the header
             self.worksheet.append(values)
         else:
             self.worksheet.append(values)
+            self.add_table_borders(letters)
 
         self.current_row_index += 1
 
@@ -94,5 +76,51 @@ class AddToXlsxPipeline:
         self.worksheet.column_dimensions['C'].width = 70
 
     def close_spider(self, spider):
+        max_row = self.worksheet.max_row
+        letters = [f'A{max_row}', f'B{max_row}', f'C{max_row}']
+
+        self.add_table_borders(letters)
+
         excel_file = f'{str(dt.now().timestamp()).replace(".", "_")}_foodbazaar.xlsx'  # File xlsx name
         self.workbook.save(excel_file)
+
+    def add_table_borders(self, letters: list[str]) -> None:
+        """
+        Add border style to the cells in `letters`.
+
+        :param letters: List of cells' names.
+        :return: None
+        """
+
+        for letter in letters:
+            self.worksheet[letter].border = Border(
+                top=Side(border_style='thin', color='000000'),
+                right=Side(border_style='thin', color='000000'),
+                bottom=Side(border_style='thin', color='000000'),
+                left=Side(border_style='thin', color='000000')
+            )
+
+    def add_header_style(self, letters: list[str]) -> None:
+        """
+        Add style to the cells of head row in the table.
+
+        :param letters: List of cells' names.
+        :return: None
+        """
+
+        for letter in letters:
+            self.worksheet[letter].fill = PatternFill(
+                'solid',
+                fgColor='dfe4ff'
+            )
+            self.worksheet[letter].font = Font(
+                bold=True,
+                name='Arial',
+                size=12
+            )
+            self.worksheet[letter].alignment = Alignment(
+                horizontal='center',
+                vertical='center'
+            )
+
+        self.add_table_borders(letters)
